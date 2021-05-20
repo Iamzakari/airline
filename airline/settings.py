2fd64c2fe56f8173fda137f4d052a6c2b8aa35ef
Zakari <41507961+Iamzakari@users.noreply.github.com>
2021-05-20 15:03:57 +0200

    Update settings.py
    Fix error in setting.py
    Registrate 'flights.apps.FlightsConfig', into INSTALLED_APPS

--- airline/settings.py
+++ airline/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'flights'
+    #'flights',
+    'flights.apps.FlightsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
